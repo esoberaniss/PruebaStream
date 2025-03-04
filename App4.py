8c7c2e0e634b87c2cfea9c0792dbe1bc683e46d4
esoberaniss <emmanualsob@gmail.com>
2025-03-03 23:25:43 -0600

    Update App4.py

--- App4.py
+++ App4.py
@@ -1,11 +1,15 @@
 import streamlit as st
 
-# Se inicializa la variable como False en cada ejecución
-verified = False  
+st.write(st.session_state)
+
+# Inicializar en session_state si no existe
+if "verified" not in st.session_state:
+    st.session_state.verified = False  
 
 if st.button("Verify"):
-    verified = True  # Si se presiona el botón, cambiamos a True
+    st.session_state.verified = True  # Guardar el estado
 
-if verified:
+if st.session_state.verified:
     st.write("Verification successful!")
-    st.selectbox("Phase",options=("Hola","Mundo"))
+    st.selectbox("Phase", options=("Hola", "Mundo"))
+
