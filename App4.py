dc85bf0de0a5b116aa96a4580284c3450227b2ef
esoberaniss <emmanualsob@gmail.com>
2025-03-03 18:59:52 -0600

    Update App4.py

--- App4.py
+++ App4.py
@@ -1,4 +1,10 @@
 import streamlit as st
 
-if st.button("Click me!"):
-    st.write("Hello, World!")
+# Se inicializa la variable como False en cada ejecución
+verified = False  
+
+if st.button("Verify"):
+    verified = True  # Si se presiona el botón, cambiamos a True
+
+if verified:
+    st.write("Verification successful!")
